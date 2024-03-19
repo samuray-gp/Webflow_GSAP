bbbc4a112b25de3f2940b419fc86a28ef230c00b
Denis <denis@MacBook-Pro-Denis.local>
2024-03-20 03:52:16 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//const firstName = 'Denis'
+//firstName = 'Sergey'
+//console.log('Hello', firstName)
+
+let firstName = 'Denis'
+firstName = 'Sergey'
+
+console.log('hello', firstName)
+
+var lastName = 'Gurkov'
+lastName = 'Babenko'
+console.log('hello', lastName)
