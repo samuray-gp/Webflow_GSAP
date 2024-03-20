568f6e720d8e795e1d2b728b5f64c0b8ada77b97
Denis <denis@MacBook-Pro-Denis.local>
2024-03-21 02:36:07 +0700

    First Commit

---
+++ lesson03/app.js
@@ -0,0 +1,28 @@
+// Number
+// bigint
+// String
+// Boolean
+// Undefined
+// Null
+// Symbol
+// Object
+
+
+const titleProject = 'Оценка строимости'
+console.log(titleProject);
+
+const screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+console.log(screensValue);
+
+const screenPrice = 20;
+console.log(screenPrice);
+
+const percentage = 40;
+console.log(percentage);
+
+const fullPrice = 1000;
+console.log(fullPrice);
+
+const Boolean = true
+console.log(Boolean);
+
