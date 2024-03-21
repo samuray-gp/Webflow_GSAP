65744b5ac76c5d59096559cceb8668e0aeba9fad
Denis <denis@MacBook-Pro-Denis.local>
2024-03-21 16:21:25 +0700

    lesson-04

--- lesson04/lesson-04.js
+++ lesson04/lesson-04.js
@@ -6,74 +6,55 @@
 // Boolean (Bool)
 
 
-let titleProject = 'Оценка строимости'
-let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
-let screenPrice = 500
-// let fullPrice = 1000
-let service1 = 'дизайн'
-let service2 = "разработка"
-let responsive = true
-let servicePrice1 = 8500
-let servicePrice2 = 10000
-// let percentage = 10
+
+let screenPrice = 1000
+let percentage = 10
 
 
-
-prompt('Название проекта?') 
+let titleProject = prompt('Название проекта') 
 console.log(titleProject);
 
-prompt('Типы экранов?') 
+let screensValue = prompt('шаблонные, с уникальным дизайном, с анимациями') 
 console.log(screensValue);
 
-prompt('нужен ли респонсивный сайт?') 
+let responsive = prompt('нужен ли респонсивный сайт?') 
 console.log(responsive);
 
-prompt('Какой сервис нужен?') 
+let service1 =  prompt('Какой сервис нужен?') 
 console.log(service1);
 
-prompt('Сколько это будет стоить?') 
+let servicePrice1 = +prompt('Сколько это будет стоить?') 
 console.log(servicePrice1);
 
-prompt('Какой еще сервис нужен?') 
+let service2 =  prompt('Какой еще сервис нужен?') 
 console.log(service2);
 
-prompt('Сколько будет стоить этот второй сервис?') 
+let servicePrice2 = +prompt('Сколько будет стоить этот второй сервис?') 
 console.log(servicePrice2);
 
-
 let fullPrice = screenPrice + servicePrice1 + servicePrice2
+console.log(fullPrice);
 
-let percentage = fullPrice / 100 * 10
-console.log(percentage);
-let servicePercentPrice = fullPrice - percentage
-console.log(servicePercentPrice);
+let percentResalt = +(fullPrice * (percentage / 100))
+console.log(percentResalt);
+
+let servicePercentPrice = fullPrice - percentResalt
+console.log(Math.ceil(servicePercentPrice));
+
 
 if (fullPrice > 50000) {
     console.log('10%');
-}  else {
+}  else if (fullPrice > 20000 && fullPrice < 50000) {
     console.log('сделаем скидку 5%');
-}  
-
-if (fullPrice = 50000) {
-    console.log('сделаем скидку 7%');
-}  
-
-
-if (fullPrice > 20000) {
-    console.log('сделаем скидку 5%');
-} else {
+} else if (fullPrice < 20000 && fullPrice > 0) {
     console.log('скидка не предусмотрена');
-}  
+} else if (fullPrice < 0) {
+    console.log('Что-то пошло не так');
+} else if (fullPrice === 0 || fullPrice === 20000 || fullPrice === 50000) {
+    console.log('В другой раз повезет');
+}
 
-if (fullPrice = 20000) {
-    console.log('сделаем скидку 2%');
-}  
+ 
 
-if (fullPrice < 0) {
-    console.log('Что то пошло не так');
-} 
 
-if (fullPrice = 0) {
-    console.log('Не сегодня');
-} 
-    
+
