0186c2a6f8f37bf5f598840e3c7312f6e77a242c
Denis <denis@MacBook-Pro-Denis.local>
2024-03-21 09:58:42 +0700

    First Commit

--- lesson03/app.js
+++ lesson03/app.js
@@ -8,21 +8,10 @@
 // Object
 
 
-const titleProject = 'Оценка строимости'
-console.log(titleProject);
-
-const screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
-console.log(screensValue);
-
-const screenPrice = 20;
-console.log(screenPrice);
-
-const percentage = 40;
-console.log(percentage);
-
-const fullPrice = 1000;
-console.log(fullPrice);
-
-const Boolean = true
-console.log(Boolean);
+let titleProject = 'Оценка строимости'
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+let screenPrice = 20
+let percentage = 40
+let fullPrice = 1000
+let Boolean = true
 
