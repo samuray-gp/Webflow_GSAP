5d0be00c4a71ded432c0960f50bc5183f837f757
Denis <denis@MacBook-Pro-Denis.local>
2024-03-21 10:19:52 +0700

    lesson04

---
+++ lesson04/lesson-04.js
@@ -0,0 +1,16 @@
+// titleProject (string)
+// screensValue (string)
+// screenPrice (num)
+// fullPrice (num)
+// screenPrice (num)
+// Boolean (Bool)
+
+
+let titleProject = 'Оценка строимости'
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+let screenPrice = 20
+let percentage = 40
+let fullPrice = 1000
+let Boolean = true
+
+console.log(titleProject);
